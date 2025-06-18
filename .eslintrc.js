1f34efedfafe03b42668ab95a43f26520bc40d7f
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-06-18 14:56:27 +0700

    Nonactive Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: "next/core-web-vitals",
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@next/next/no-img-element": "off",
+  },
+};
