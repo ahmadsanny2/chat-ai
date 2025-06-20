888fc630a8cbefb25122c6201b0a13b390840a9b
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-06-20 15:27:06 +0700

    Fixed validated type data

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -4,18 +4,21 @@ export async function POST(req: Request) {
   const { prompt } = await req.json();
 
   try {
-    const response = await fetch("https://api.openai.com/v1/images/generations", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY!}`,
-      },
-      body: JSON.stringify({
-        prompt,
-        n: 1,
-        size: "512x512",
-      }),
-    });
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY!}`,
+        },
+        body: JSON.stringify({
+          prompt,
+          n: 1,
+          size: "512x512",
+        }),
+      }
+    );
 
     const data = await response.json();
 
@@ -30,8 +33,9 @@ export async function POST(req: Request) {
     }
 
     return NextResponse.json({ image_url: data.data[0].url });
-  } catch (error: unknown) {
-    console.error("❌ ERROR:", error.message);
-    return NextResponse.json({ error: error.message }, { status: 500 });
+  } catch (error) {
+    const msg = error instanceof Error ? error.message : "Terjadi kesalahan.";
+    console.error("❌ ERROR:", msg);
+    return NextResponse.json({ error: msg }, { status: 500 });
   }
 }
